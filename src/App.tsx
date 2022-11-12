7e0e9fe3d2577be172281f0e285030fee430708d
Rahimuddin Mohammad <mdrahiem@gmail.com>
2022-11-12 07:33:09 +0100

    Initial project setup with vite, react and scss

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>
+        Welcome to puzzle <span>Yellow</span>
+      </h1>
+    </div>
+  );
+}
+
+export default App;
