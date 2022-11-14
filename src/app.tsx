3fd1f619030989186fd02bea5cdc3220a607ddb2
Rahimuddin Mohammad <mdrahiem@gmail.com>
2022-11-14 09:03:32 +0100

    global ignore case set to false

---
+++ src/app.tsx
@@ -0,0 +1,71 @@
+import { useMemo, useState, useEffect } from "react";
+import "./scss/app.scss";
+import Block from "./components/block";
+import EnterGameSizeForm from "./components/enter-game-size";
+import NumberOfMoves from "./components/number-of-moves";
+import WinningMessage from "./components/winning-message";
+import { IPuzzleData } from "./types";
+import { getShuffledBlocks } from "./utils";
+
+const initPuzzleData: IPuzzleData = {
+  numberOfBlocks: 0,
+  gameStarted: false,
+  blocks: [],
+  isWon: false,
+  numberOfMoves: 0,
+};
+
+function App() {
+  const memoizedInitData = useMemo(() => initPuzzleData, [initPuzzleData]);
+  const [puzzleData, setPuzzleData] = useState<IPuzzleData>(memoizedInitData);
+
+  useEffect(() => {
+    setPuzzleData((puzzleData) => ({
+      ...puzzleData,
+      blocks: getShuffledBlocks(puzzleData.numberOfBlocks),
+    }));
+  }, [puzzleData.numberOfBlocks]);
+
+  return (
+    <div className="app">
+      <h1 className="app-title">Welcome to puzzle</h1>
+      {puzzleData.gameStarted ? (
+        <>
+          <p className="puzzle-tip" data-testid="puzzle-tip">
+            💡 You need to click on a block to move!
+          </p>
+          {puzzleData.numberOfMoves > 0 && (
+            <NumberOfMoves moves={puzzleData.numberOfMoves} />
+          )}
+          <div
+            className="puzzle-container"
+            style={{
+              width: puzzleData.numberOfBlocks * 100,
+              height: puzzleData.numberOfBlocks * 100,
+            }}
+          >
+            {puzzleData.isWon && (
+              <WinningMessage messageSize={puzzleData.numberOfBlocks * 100} />
+            )}
+            {puzzleData.blocks.map((block) => (
+              <Block
+                blockId={block}
+                key={block}
+                puzzleData={puzzleData}
+                setPuzzleData={setPuzzleData}
+                totalBlocks={puzzleData.blocks.length}
+                blockSize={
+                  (puzzleData.numberOfBlocks * 100) / puzzleData.numberOfBlocks
+                }
+              />
+            ))}
+          </div>
+        </>
+      ) : (
+        <EnterGameSizeForm setPuzzleData={setPuzzleData} />
+      )}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/block.tsx
@@ -0,0 +1,58 @@
+import { MouseEvent, useCallback } from "react";
+import { IBlockProps } from "../types";
+import { checkIsWon, checkSwappable, getSwappedBlocks } from "../utils";
+
+function Block({
+  blockId,
+  totalBlocks,
+  blockSize,
+  puzzleData,
+  setPuzzleData,
+}: IBlockProps) {
+  const isEmptyBlock = blockId === totalBlocks - 1;
+
+  const handleBlockClick = useCallback(
+    (event: MouseEvent<HTMLDivElement>, blockId: number) => {
+      event.preventDefault();
+      // check whether the block is swappable
+      const isSwappableBlock = checkSwappable(
+        blockId,
+        puzzleData.blocks,
+        puzzleData.numberOfBlocks
+      );
+      if (isSwappableBlock) {
+        // increase the move count
+        setPuzzleData((puzzleData) => ({
+          ...puzzleData,
+          numberOfMoves: puzzleData.numberOfMoves + 1,
+        }));
+        const swappedBlocks = [...getSwappedBlocks(blockId, puzzleData.blocks)];
+        setPuzzleData((puzzleData) => ({
+          ...puzzleData,
+          blocks: swappedBlocks,
+        }));
+        // check whether puzzle is solved
+        const isPuzzleSolved = checkIsWon(swappedBlocks);
+        if (isPuzzleSolved) {
+          setPuzzleData((puzzleData) => ({
+            ...puzzleData,
+            isWon: isPuzzleSolved,
+          }));
+        }
+      }
+    },
+    [blockId]
+  );
+  return (
+    <div
+      className="puzzle-block"
+      data-testid="puzzle-block"
+      onClick={(e) => handleBlockClick(e, blockId)}
+      style={{ width: blockSize, height: blockSize }}
+    >
+      {!isEmptyBlock ? blockId + 1 : ""}
+    </div>
+  );
+}
+
+export default Block;
